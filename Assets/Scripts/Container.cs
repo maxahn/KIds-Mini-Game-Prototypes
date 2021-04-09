90873f2b7c390d528ee411933586c19463f482a8
ahnmax <ahnmaximus@gmail.com>
2021-04-08 21:02:34 -0700

    project init with assets and basic chest animations

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prize;
+    [SerializeField]
+    private Animator animator;
+    private bool isOpened;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        isOpened = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isOpened)
+        {
+            animator.Play("Open Chest");
+            this.isOpened = true; 
+        }
+    }
+}
